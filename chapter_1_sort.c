72b4d9ab1e1ba99a07a3e3044be932e7db447cac
mossnodie <moss@nodie.ink>
2018-12-27 19:11:20 +0800

    update

--- chapter_1_sort.c
+++ chapter_1_sort.c
@@ -78,14 +78,61 @@ int * bubble_sort(int * p_number_arr,int length){
 	
 }
 
+void quick_sort(int pl,int ph,int * p_number_arr){
+	
+	int left  = pl;
+	int right = ph;
+	
+	if(left > right){
+		return;
+	}
+	
+	int base_number = p_number_arr[left];
+	
+	while(ph!=pl){
+		
+		while(p_number_arr[ph] >= base_number && ph>pl){
+			--ph;	
+		}
+		while(p_number_arr[pl] <= base_number && ph>pl){
+			++pl;	
+		}		
+		
+		if(ph>pl){
+			int t = p_number_arr[ph];
+			p_number_arr[ph] = p_number_arr[pl];
+			p_number_arr[pl] = t;
+		}
+		
+	}
+	
+	p_number_arr[left] = p_number_arr[pl];
+	p_number_arr[pl] = base_number;
+	
+	quick_sort(left,pl-1,p_number_arr);
+	quick_sort(ph+1,right,p_number_arr);
+	
+//	int _number_arr[8] = {8,7,6,5,4,3,2,1};
+//	
+//	quick_sort(0,7,_number_arr);
+//	
+//	for(int i = 0 ;i < 8; i++){
+//		printf("%d ",_number_arr[i]);
+//	}
+//	
+//	return 0;
+
+	
+}
+
 int main() {
 	
 	int _number_arr[8] = {8,7,6,5,4,3,2,1};
 	
-	int * _res_arr = bubble_sort(_number_arr,8);
+	quick_sort(0,7,_number_arr);
 	
 	for(int i = 0 ;i < 8; i++){
-		printf("%d ",_res_arr[i]);
+		printf("%d ",_number_arr[i]);
 	}
 	
 	return 0;
